18ee64a6076b6a2fa4f2f2e3a8b89c550c0b0c6b
FireFlyMEki <72804777+FireFlyMEki@users.noreply.github.com>
2026-01-06 22:43:30 +0300

    1

---
+++ safonchik40331Labs.Domain/Entites/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace safonchik40331Labs.Domain.Entites
+{
+    public class Author
+    {
+        public int Id { get; set; } // id автора
+
+        public string Name { get; set; } // имя автора
+
+        public string NormalizedName { get; set; } // нормализованное имя для URL
+    }
+}
---
+++ safonchik40331Labs.Domain/Entites/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace safonchik40331Labs.Domain.Entites
+{
+    public class Book
+    {
+        public int Id { get; set; } // id книги
+
+        public string Title { get; set; } // название книги
+
+        public string Description { get; set; } // краткое описание
+
+        public int Rating { get; set; } // рейтинг книги 
+
+        public string? CoverImage { get; set; } // путь к файлу обложки
+
+        // Навигационные свойства
+        /// <summary>
+        /// автор книги
+        /// </summary>
+        public int AuthorId { get; set; }
+        public Author? Author { get; set; }
+    }
+}
